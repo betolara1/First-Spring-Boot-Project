232b525fa47b3fced3df56e770b347020386ff73
roberto <robertokantovitzlara@hotmail.com>
2025-12-09 08:59:46 -0300

    usando service para retornar no controller

--- src/main/java/com/ranetech/demo/controller/HelloController.java
+++ src/main/java/com/ranetech/demo/controller/HelloController.java
@@ -3,13 +3,26 @@ package com.ranetech.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.ranetech.demo.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-    @GetMapping
+    // existe 2 maneiras de injetar o HelloService no HelloController
+
+    // 1 - criar um construtor para o HelloController que recebe o HelloService como parametro
+    /*private final HelloService helloService;
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }*/
+
+    //2 - usar a anotacao @Autowired para injetar o HelloService no HelloController
+    @Autowired
+    private HelloService helloService;
     
+    @GetMapping
     public String hello() {
-        return "Hello World from Spring Boot";
+        return helloService.helloMessage("Roberto");
     }
 }
---
+++ src/main/java/com/ranetech/demo/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.ranetech.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String helloMessage(String name) {
+        return "Hello world, my name is " + name;
+    }
+}
